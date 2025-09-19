5b9e8050a4042eb7a6635308b6dde82b8c816fd4
Joshua Plischek <plischekjoshua@web.de>
2025-09-19 22:57:41 +0200

    add style to the landingpage and make the navbar sticky

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from "./hero/hero.component";
+import { ServicesComponent } from "./services/services.component";
+import { AboutUsComponent } from "./about-us/about-us.component";
+import { ContactComponent } from "./contact/contact.component";
 
 @Component({
   selector: 'app-main-page',
-  imports: [HeroComponent],
+  imports: [HeroComponent, ServicesComponent, AboutUsComponent, ContactComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
