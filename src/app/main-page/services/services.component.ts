db55cbf46778174eb38ee036205b50c539d04c94
Joshua Plischek <plischekjoshua@web.de>
2025-09-23 12:28:29 +0200

    edit the extra service comp

--- src/app/main-page/services/services.component.ts
+++ src/app/main-page/services/services.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ExtraServicesComponent } from './extra-services/extra-services.component';
+import { ElementsComponent } from './elements/elements.component';
 
 @Component({
   selector: 'app-services',
-  imports: [ExtraServicesComponent],
+  imports: [ExtraServicesComponent, ElementsComponent],
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss'
 })
