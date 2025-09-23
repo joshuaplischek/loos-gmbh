f9c8dd88569e776e20234173a9998f1926c9973a
Joshua Plischek <plischekjoshua@web.de>
2025-09-23 17:50:30 +0200

    add footer and about us section

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -3,10 +3,11 @@ import { HeroComponent } from "./hero/hero.component";
 import { ServicesComponent } from "./services/services.component";
 import { AboutUsComponent } from "./about-us/about-us.component";
 import { ContactComponent } from "./contact/contact.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-main-page',
-  imports: [HeroComponent, ServicesComponent, AboutUsComponent, ContactComponent],
+  imports: [HeroComponent, ServicesComponent, AboutUsComponent, ContactComponent, FooterComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
