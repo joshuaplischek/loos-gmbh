a0c6ce9d3d1b76cf57fb037edbe0fecde82932c6
Joshua Plischek <plischekjoshua@web.de>
2025-09-24 15:50:47 +0200

    make project responsive

--- src/app/main-page/hero/hero.component.ts
+++ src/app/main-page/hero/hero.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
-import { NavBarComponent } from "../nav-bar/nav-bar.component";
+import { Component, HostListener } from '@angular/core';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, NgClass],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+  styleUrl: './hero.component.scss',
 })
 export class HeroComponent {
+  public getScreenWidth: any;
+  public getScreenHeight: any;
+  isMobile: boolean | undefined;
 
+  ngOnInit() {
+    this.getScreenWidth = window.innerWidth;
+    this.getScreenHeight = window.innerHeight;
+    this.onWindowResize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.getScreenWidth = window.innerWidth;
+    this.getScreenHeight = window.innerHeight;
+    if (this.getScreenWidth <= 630) {
+      this.isMobile = true;
+    } else {
+      this.isMobile = false;
+    }
+  }
 }
--- src/app/main-page/services/services.component.ts
+++ src/app/main-page/services/services.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { ExtraServicesComponent } from './extra-services/extra-services.component';
 import { ElementsComponent } from './elements/elements.component';
 
@@ -9,5 +9,24 @@ import { ElementsComponent } from './elements/elements.component';
   styleUrl: './services.component.scss'
 })
 export class ServicesComponent {
+    public getScreenWidth: any;
+    public getScreenHeight: any;
+    isMobile: boolean | undefined;
 
+  ngOnInit() {
+      this.getScreenWidth = window.innerWidth;
+      this.getScreenHeight = window.innerHeight;
+      this.onWindowResize()
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.getScreenWidth = window.innerWidth;
+    this.getScreenHeight = window.innerHeight;
+    if (this.getScreenWidth <= 800) {
+      this.isMobile = true
+    } else {
+      this.isMobile = false
+    }
+  }
 }
