c3d30bf2071e22d55672495a7c03a35a6fd90973
Joshua Plischek <plischekjoshua@web.de>
2025-09-29 14:55:45 +0200

    add privacy policy

--- src/app/main-page/imprint/imprint.component.ts
+++ src/app/main-page/imprint/imprint.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from "../nav-bar/nav-bar.component";
 import { MobileService } from '../../services/mobile.service';
+import { routes } from '../../app.routes';
+import { RouterLink } from '@angular/router';
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-imprint',
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, FooterComponent],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
--- src/app/main-page/privacy-plicy/privacy-plicy.component.ts
+++ src/app/main-page/privacy-plicy/privacy-plicy.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from "../nav-bar/nav-bar.component";
+import { FooterComponent } from "../footer/footer.component";
+import { MobileService } from '../../services/mobile.service';
 
 @Component({
   selector: 'app-privacy-plicy',
-  imports: [],
+  imports: [NavBarComponent, FooterComponent],
   templateUrl: './privacy-plicy.component.html',
   styleUrl: './privacy-plicy.component.scss'
 })
 export class PrivacyPlicyComponent {
-
+  constructor(public mobile: MobileService){}
 }
